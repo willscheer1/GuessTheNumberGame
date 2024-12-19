52fdc1cd3c699c5f3a235f880c8841659c3542f0
-- <-->
2024-12-19 08:27:10 -0600

    project setup

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Guess the Number game where a random number is generated and the user has n number of tries to guess that number.
+The game has 3 different difficulties, easy, intermediate, and hard, where the range of possible numbers and number of
+guesses allowed increases respectively. An option for hints is available to tell the user whether the mystery number
+is higher or lower than the given guess. On game end, the user has the option to play again, change the difficulty,
+or exit the game.
+""" 
+
+
+
+
+if __name__ == "__main__":
+    pass
